cb849024873167e6b21d5dfcfa70985697f15ba3
Johannes Klumpe <klumpe@live.de>
2016-07-01 03:11:35 +0200

    Activated asset hashes and directory indexes

--- config.rb
+++ config.rb
@@ -36,9 +36,11 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
+
+  # Append a hash to asset urls (make sure to use the url helpers)
+  activate :asset_hash
+
+  activate :directory_indexes
 end

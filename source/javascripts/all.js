95f73784805b525d8328330df735f2e5e17803cf
Julrob <36548659+Julrob@users.noreply.github.com>
2018-03-09 16:54:34 +0100

    Update all.js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -22,8 +22,8 @@ $(document).ready(function() {
         };
 
         var getTimeRemaining = function(endtime) {
-            var now = new Date();
-            var then = new Date(endtime);
+            var now = Date.parse(new Date());
+            var then = Date.parse(endtime);
             infoAbend(new Date(endtime));
             if (now >= then) {
                 return {
